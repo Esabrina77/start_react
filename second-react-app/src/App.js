fcc13afe83dc0b18972b40ed6d32fcf2e3de79fd
Esabrina77 <sabrina.eloundou@ynov.com>
2024-06-26 19:58:41 +0200

    2nd REACT APP updated

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,22 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './App.css';
 
-const Title = ({ content }) => <h1 id="title">{content}</h1>
 
-const Component = ({ title, onClick }) =>
-  <button onClick={() => onClick(title)}>Apprendre {title}</button>
+// const Title = ({ content }) => <h1 id="title">{content}</h1>
+
+// const Component = ({ title, onClick }) =>
+//   <button onClick={() =>
+//     onClick(title)}>Apprendre {title}</button>
 
 function App() {
-  const [title, setTitle] = useState("React");
+  const [count, setCount] = useState(0)
+  const reset = () => {}
+  useEffect(() => {
+    setCount(0)
+  }, [])
 
-  const handleOnClick = (lib) => setTitle(lib)
+  // const [title, setTitle] = useState("React");
+  // const handleOnClick = (lib) => setTitle(lib)
+
+  // useEffect(() => {
+  //   document.title = `learn ${title}`
+  // }, [title])
+
   return (
     <div className="App">
       <header className="App-header">
-        <Title content={title} />
+        <h1 className="result">Counter</h1>
+        <p>You clicked {count}times </p>
+        <button onClick={() => setCount(count => count + 1)}>CLICK</button>
+        <button onClick={reset}>reset</button>
+
+        {/* <br></br>
+       <Title content={title} />
         <Component onClick={handleOnClick} title="React"></Component>
         <Component onClick={handleOnClick} title="Angular" ></Component>
-        <Component onClick={handleOnClick} title="Vue"></Component>
+        <Component onClick={handleOnClick} title="Vue"></Component> */}
 
       </header>
     </div>
